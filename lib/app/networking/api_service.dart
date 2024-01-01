f25fd8e22b2d36678e14955cb465627bb3a433b7
Anthony <ants52@aol.com>
2024-01-01 04:10:13 +0000

    v5.16.0

--- lib/app/networking/api_service.dart
+++ lib/app/networking/api_service.dart
@@ -31,4 +31,72 @@ class ApiService extends NyApiService {
         request: (request) => request.get("/endpoint-path"),
     );
   }
+
+  /* Helpers
+  |--------------------------------------------------------------------------
+  */
+
+
+  /* Authentication Headers
+  |--------------------------------------------------------------------------
+  | Set your auth headers
+  | Authenticate your API requests using a bearer token or any other method
+  |--------------------------------------------------------------------------
+  */
+
+  // @override
+  // Future<RequestHeaders> setAuthHeaders(RequestHeaders headers) async {
+  //   String? myAuthToken = await StorageKey.userToken.read();
+  //   if (myAuthToken != null) {
+  //     headers.addBearerToken( myAuthToken );
+  //   }
+  //   return headers;
+  // }
+
+
+  /* Should Refresh Token
+  |--------------------------------------------------------------------------
+  | Check if your Token should be refreshed
+  | Set `false` if your API does not require a token refresh
+  |--------------------------------------------------------------------------
+  */
+
+  // @override
+  // Future<bool> shouldRefreshToken() async {
+  //   return false;
+  // }
+
+
+  /* Refresh Token
+  |--------------------------------------------------------------------------
+  | If `shouldRefreshToken` returns true then this method
+  | will be called to refresh your token. Save your new token to
+  | local storage and then use the value in `setAuthHeaders`.
+  |--------------------------------------------------------------------------
+  */
+
+  // @override
+  // refreshToken(Dio dio) async {
+   // dynamic response = (await dio.get("https://example.com/refresh-token")).data();
+   // Save the new token
+   //  await StorageKey.userToken.store(response['token']);
+  // }
+
+
+  /* Display a error
+  |--------------------------------------------------------------------------
+  | This method is only called if you provide the API service
+  | with a [BuildContext]. Example below:
+  | api<ApiService>(
+  |        request: (request) => request.myApiCall(),
+  |         context: context);
+  |--------------------------------------------------------------------------
+  */
+
+  // displayError(DioException dioException, BuildContext context) {
+  //   showToastNotification(context,
+  //       title: "Error",
+  //       description: dioException.message ?? "",
+  //       style: ToastNotificationStyleType.DANGER);
+  // }
 }
